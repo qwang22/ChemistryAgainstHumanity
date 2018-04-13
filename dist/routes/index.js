d137a768a59cfd14116a3730d8f4fabb2f122229
Carlos Vargas <csv17@live.unc.edu>
2018-04-13 14:07:24 -0400

    leaderboard

--- dist/routes/index.js
+++ dist/routes/index.js
@@ -27,6 +27,9 @@ class IndexRoute extends route_1.BaseRoute {
         router.post("/addReaction", (req, res, next) => {
             new IndexRoute().addReaction(req, res, next);
         });
+        router.post("/generateCards", (req, res, next) => {
+            new IndexRoute().generateCards(req, res, next);
+        });
     }
     constructor() {
         super();
@@ -57,11 +60,20 @@ class IndexRoute extends route_1.BaseRoute {
     }
     getImage(req, res, next) {
         var json_obj = {};
-        var src_urls = Object.keys(req.body).map(function (key) {
-            var chem = req.body[key];
-            var encoded = encodeURIComponent(chem);
-            var imgURL = "http://opsin.ch.cam.ac.uk/opsin/" + encoded + ".png";
-            json_obj[key] = imgURL;
+        Object.keys(req.body).map(function (key) {
+            var chem = req.body[key]['name'];
+            var img_src;
+            if (typeof req.body[key]['depiction'] == 'undefined') {
+                var encoded = encodeURIComponent(chem);
+                img_src = "http://opsin.ch.cam.ac.uk/opsin/" + encoded + ".png";
+            }
+            else {
+                img_src = req.body[key]['depiction'];
+            }
+            json_obj[key] = {
+                name: chem,
+                depiction: img_src
+            };
         });
         var response = JSON.stringify(json_obj);
         console.log("response = " + response);
@@ -72,14 +84,54 @@ class IndexRoute extends route_1.BaseRoute {
         this.render(req, res, "game", options);
     }
     addReaction(req, res, next) {
+        console.log(req.body);
+        var host = "mongodb://localhost:27017";
+        mongo.MongoClient.connect(host, function (err, db) {
+            if (err)
+                throw err;
+            var dbo = db.db("chemistryagainsthumanity");
+            var reaction_entry = {
+                reactant: req.body.reactant,
+                reagent: req.body.reagent,
+                product: req.body.product,
+                active: true
+            };
+            dbo.collection("reactions").insertOne(reaction_entry, function (err, res) {
+                if (err)
+                    throw err;
+                var rid = res['ops'][0]['_id'];
+                console.log('1 reaction inserted into reactions table');
+                var cards_entry = new Array();
+                Object.keys(req.body).map(function (key) {
+                    var card_obj = {
+                        rid: rid,
+                        type: key,
+                        front: req.body[key]['front'],
+                        back: req.body[key]['back']
+                    };
+                    cards_entry.push(card_obj);
+                });
+                dbo.collection("cards").insertMany(cards_entry, function (err, res) {
+                    if (err)
+                        throw err;
+                    console.log('3 cards inserted into cards table');
+                });
+            });
+        });
+        var response = { status: "done" };
+        res.send(response);
+    }
+    generateCards(req, res, next) {
         mongo.MongoClient.connect("mongodb://localhost:27017", function (err, db) {
             if (err)
                 throw err;
             var dbo = db.db("chemistryagainsthumanity");
-            dbo.collection("cards").insertOne(req.body.card1, function (err, res) {
+            dbo.collection("cards").find({}).toArray(function (err, res2) {
                 if (err)
                     throw err;
-                console.log("1 document inserted");
+                console.log(res2);
+                var response = JSON.stringify(res2);
+                res.send(response);
             });
         });
     }
